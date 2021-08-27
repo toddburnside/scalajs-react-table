19b85f2cf38e52b9d71fe2623cb3b047456d2f45
Todd Burnside <tburnside@gemini.edu>
2021-08-27 11:35:50 -0700

    Update scalafmt-core to 3.0.1
    Change some scaladoc formatting to preserve indenting.

--- facade/src/main/scala/reactST/reactTable/HTMLTableBuilder.scala
+++ facade/src/main/scala/reactST/reactTable/HTMLTableBuilder.scala
@@ -22,22 +22,27 @@ object HTMLTableBuilder {
   /**
    * Create a table component based on the configured plugins of a `TableMaker`.
    *
-   * @param tableMaker The TableMaker providing the useTable hook.
-   * @param headerCellFn A function to use for creating the <th> elements. Simple ones are provided by the TableMaker object.
-   * @param tableClass An optional CSS class to apply to the table element
-   * @param rowClassFn An option function that takes the index and the rowData and creates a class for the row. Relying on index does not work well for sortable tables since it is the unsorted index.
-   * @param footer An optional <tfoot> element. (see note below)
+   * @param tableMaker
+   *   The TableMaker providing the useTable hook.
+   * @param headerCellFn
+   *   A function to use for creating the <th> elements. Simple ones are provided by the TableMaker
+   *   object.
+   * @param tableClass
+   *   An optional CSS class to apply to the table element
+   * @param rowClassFn
+   *   An option function that takes the index and the rowData and creates a class for the row.
+   *   Relying on index does not work well for sortable tables since it is the unsorted index.
+   * @param footer
+   *   An optional <tfoot> element. (see note below)
    *
    * Notes:
-   * If optimization is required, parts of the options and the data
-   *   should be memoized and only change when necessary. See react-table
-   *   documentation for details.
-   * The headerCellFn is expected to return a <th> element. The function
-   *   should make use of the column instance "props" methods.
-   * Table footers are fully "Build It Yourself" since it is often useful
-   *   for table footers to span multiple columns and react-table does
-   *   not support that. At some point, something similar for an "extra"
-   *   header row might be useful since header groups have some issues.
+   *   - If optimization is required, parts of the options and the data should be memoized and only
+   *     change when necessary. See react-table documentation for details.
+   *   - The headerCellFn is expected to return a <th> element. The function should make use of the
+   *     column instance "props" methods.
+   *   - Table footers are fully "Build It Yourself" since it is often useful for table footers to
+   *     span multiple columns and react-table does not support that. At some point, something
+   *     similar for an "extra" header row might be useful since header groups have some issues.
    */
   // format: off
   def buildComponent[D,
@@ -81,30 +86,35 @@ object HTMLTableBuilder {
     }
 
   /**
-   * Create a virtualized table based on the configured plugins of a `TableMaker`.
-   * The plugins MUST include withBlockLayout.
+   * Create a virtualized table based on the configured plugins of a `TableMaker`. The plugins MUST
+   * include withBlockLayout.
    *
-   * @param tableMaker The TableMaker providing the useTable hook, configured with withBlockLayout.
-   * @param options The table options to use for the table.
-   * @param bodyHeight The optional height of the table body (virtualized portion) in px. See note.
-   * @param headerCellFn See note. A function to use for creating the header elements. Simple ones are provided by the TableMaker object.
-   * @param tableClass An optional CSS class to apply to the table element
-   * @param rowClassFn An option function that takes the index and the rowData and creates a class for the row. Relying on index does not work well for sortable tables since it is the unsorted index.
+   * @param tableMaker
+   *   The TableMaker providing the useTable hook, configured with withBlockLayout.
+   * @param options
+   *   The table options to use for the table.
+   * @param bodyHeight
+   *   The optional height of the table body (virtualized portion) in px. See note.
+   * @param headerCellFn
+   *   See note. A function to use for creating the header elements. Simple ones are provided by the
+   *   TableMaker object.
+   * @param tableClass
+   *   An optional CSS class to apply to the table element
+   * @param rowClassFn
+   *   An option function that takes the index and the rowData and creates a class for the row.
+   *   Relying on index does not work well for sortable tables since it is the unsorted index.
    *
    * Notes:
-   * Virtualized tables are not built from html table elements such as
-   * <table>, <tr>, etc. The virtualization requires they be made from
-   * <div> elements and styled as a table. This method adds a class to
-   * each div that corresponds to it's role, such as "table", "thead",
-   * "tr", etc. The "tr" div is currently wrapped inside an additional
-   * div that handles the virtualization.
-   * This means that headerCellFn must also return a <div> instead
-   * of a <td>, and should probably also have a class of "td". The basic
-   * header functions in TableMaker take a boolean flag to handle this.
-   *
-   * The body height must be set to a value either through the bodyHeight
-   * parameter or via CSS or the body will collapse to nothing. In CSS, you
-   * MUST NOT use relative values like "100%" or it won't work.
+   *   - Virtualized tables are not built from html table elements such as <table>, <tr>, etc. The
+   *     virtualization requires they be made from <div> elements and styled as a table. This method
+   *     adds a class to each div that corresponds to it's role, such as "table", "thead", "tr",
+   *     etc. The "tr" div is currently wrapped inside an additional div that handles the
+   *     virtualization. This means that headerCellFn must also return a <div> instead of a <td>,
+   *     and should probably also have a class of "td". The basic header functions in TableMaker
+   *     take a boolean flag to handle this.
+   *   - The body height must be set to a value either through the bodyHeight parameter or via CSS
+   *     or the body will collapse to nothing. In CSS, you MUST NOT use relative values like "100%"
+   *     or it won't work.
    */
   // format: off
   def buildComponentVirtualized[D,
@@ -169,11 +179,13 @@ object HTMLTableBuilder {
     }
 
   /**
-   * A function to use in the builder methods for the
-   * headerCellFn parameter. This is a a basic header.
+   * A function to use in the builder methods for the headerCellFn parameter. This is a a basic
+   * header.
    *
-   * @param cellClass An optional CSS class to apply the the <th>.
-   * @param useDiv True to use a <div> instead of a <th>. Needed for tables withBlockLayout.
+   * @param cellClass
+   *   An optional CSS class to apply the the <th>.
+   * @param useDiv
+   *   True to use a <div> instead of a <th>. Needed for tables withBlockLayout.
    */
   def basicHeaderCellFn(
     cellClass: Css = Css.Empty,
@@ -182,13 +194,14 @@ object HTMLTableBuilder {
     col => headerCell(useDiv)(props2Attrs(col.getHeaderProps()), cellClass, col.renderHeader)
 
   /**
-   * A function to use in the builder methods for the headerCellFn
-   * parameter. This header will make the maker sortable by clicking
-   * on the header. Will only compile if the column instance is a subtype
-   * of UseSortByColumnPros.
+   * A function to use in the builder methods for the headerCellFn parameter. This header will make
+   * the maker sortable by clicking on the header. Will only compile if the column instance is a
+   * subtype of UseSortByColumnPros.
    *
-   * @param cellClass An optional CSS class to apply to the <th>.
-   * @param useDiv True to use a <div> instead of a <th>. Needed for tables withBlockLayout.
+   * @param cellClass
+   *   An optional CSS class to apply to the <th>.
+   * @param useDiv
+   *   True to use a <div> instead of a <th>. Needed for tables withBlockLayout.
    * @return
    */
   def sortableHeaderCellFn(
@@ -216,11 +229,13 @@ object HTMLTableBuilder {
     }
 
   /**
-   * A function to use in the builder methods for the
-   * footerCellFn parameter. This is a a basic footer.
+   * A function to use in the builder methods for the footerCellFn parameter. This is a a basic
+   * footer.
    *
-   * @param cellClass An optional CSS class to apply the the <th>.
-   * @param useDiv True to use a <div> instead of a <th>. Needed for tables withBlockLayout.
+   * @param cellClass
+   *   An optional CSS class to apply the the <th>.
+   * @param useDiv
+   *   True to use a <div> instead of a <th>. Needed for tables withBlockLayout.
    */
   def basicFooterCellFn(
     cellClass: Css = Css.Empty,
